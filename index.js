037367b9f8020fc6adf84c6bca6220ee41e0ce3d
Adam Khodier <khodieradam1297@gmail.com>
2023-12-02 21:07:06 -0500

    one more mac test

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ function sayHello() {
     console.log("Hello, World!!!");
 }
 
+function sayCodingOnMac() {
+    console.log("Final test for your mac os netowork env!")
+}
+
 sayHello();
+sayCodingOnMac();
